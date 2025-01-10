950b74fc6285bbf5d4ef3e600022d6cde3023dc1
theorigianlhats <95531511+theoriginalhats@users.noreply.github.com>
2025-01-10 14:02:02 -0500

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker installing.');
-    self.skipWaiting();
-});
 
-self.addEventListener('activate', event => {
-    console.log('Service Worker activated.');
-});
-
-self.addEventListener('fetch', event => {
-    console.log('Fetching:', event.request.url);
-});
